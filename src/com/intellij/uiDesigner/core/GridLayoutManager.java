da6208b36d8ee538d455ab40d0418a78fdf17aaf
ivan4000 <sxln1@mail.ru>
2018-11-02 18:49:02 +0300

    JL111Layout

---
+++ src/com/intellij/uiDesigner/core/GridLayoutManager.java
@@ -0,0 +1,34 @@
+package com.intellij.uiDesigner.core;
+
+import java.awt.*;
+
+public class GridLayoutManager implements LayoutManager {
+    public GridLayoutManager(int i, int i1, Insets insets, int i2, int i3) {
+        System.out.println(0);
+    }
+
+    @Override
+    public void addLayoutComponent(String name, Component comp) {
+
+    }
+
+    @Override
+    public void removeLayoutComponent(Component comp) {
+
+    }
+
+    @Override
+    public Dimension preferredLayoutSize(Container parent) {
+        return null;
+    }
+
+    @Override
+    public Dimension minimumLayoutSize(Container parent) {
+        return null;
+    }
+
+    @Override
+    public void layoutContainer(Container parent) {
+
+    }
+}
